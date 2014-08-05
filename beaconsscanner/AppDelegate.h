3821752256998c2f9e00e43705bf6490ae222fc9
Atish Narlawar <atish.narlawar@gmail.com>
2014-08-05 12:50:02 -0400

    Initial Commit

---
+++ beaconsscanner/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  beaconsscanner
+//
+//  Created by HUGE | Atish Narlawar on 8/5/14.
+//  Copyright (c) 2014 HugeInc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ beaconsscanner/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  beaconsscanner
+//
+//  Created by HUGE | Atish Narlawar on 8/5/14.
+//  Copyright (c) 2014 HugeInc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
